846cae5fd2bde7e9397afc85c774629b0d5eb091
bbiiss <bbiiss@mail.ru>
2022-01-15 07:03:02 +0300

    commit 10

--- src/test/java/cloveri360/api/tests/GetTests.java
+++ src/test/java/cloveri360/api/tests/GetTests.java
@@ -1,30 +1,33 @@
 package cloveri360.api.tests;
 
-import cloveri360.api.tests.BaseTest;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
 
 class GetTests extends BaseTest {
 
-
-
     @Test
     void getAccountTest() {
+
         given()
                 .header("Authorization", token)
                 .log()
                 .ifValidationFails()
                 .expect()
-                .body("success", CoreMatchers.is(true))
+//                .body("success", CoreMatchers.is(true)) // Перенесли в responseSpecification
                 .body("data.url", CoreMatchers.equalTo(username))
                 .when()
-                .get("account/" + username)// ("/account/{username}", username) так лучше, работает быстрее, чем конкатинация (сложение) строк
-                .prettyPeek()
-                .then()
-                .statusCode(200);
+                .get("/account/" + username)// ("/account/{username}", username) так лучше, работает быстрее, чем конкатинация (сложение) строк
+                .prettyPeek();
+//                .then()
+//                .statusCode(200);
     }
 }
--- src/test/java/cloveri360/api/tests/PostTests.java
+++ src/test/java/cloveri360/api/tests/PostTests.java
@@ -1,26 +1,30 @@
 package cloveri360.api.tests;
 
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 
-public class PostTests extends BaseTest {
+class PostTests extends BaseTest {
 
-    @Test
-    void createDepartmentLevel_2Test() {
+    @DisplayName("createDepartmentLevel_2")
+    @ParameterizedTest
+    @ValueSource(strings = {"bmp","jpg", "png", "tif"})
+    void createDepartmentLevel_2Test(String word) {
 
         imageDeleteHash = given()
-                .header("Authorization", token)
-                .body(new File(xpathImageHD + "png")) // в теле передаем файл. По сети передается в виде потока
+                .header("Authorization", token) // перенесли в requestSpecification (см. BaseTest)
+                .body(new File(xpathImageHD + word)) // в теле передаем файл. По сети передается в виде потока
                 .expect()
                 .statusCode(200)
                 .when()
-                .post("image")
+                .post("/image")
                 .prettyPeek()
                 .jsonPath()
                 .get("data.deletehash");
@@ -32,7 +36,7 @@ public class PostTests extends BaseTest {
         given()
                 .header("Authorization", token)
                 .when()
-                .delete("image/{imageHash}", imageDeleteHash)
+                .delete("/image/{imageHash}", imageDeleteHash)
                 .then()
                 .statusCode(200);
     }
