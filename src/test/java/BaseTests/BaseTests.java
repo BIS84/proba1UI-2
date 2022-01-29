6d0f729798c0747f36a2b945aefe1de2e40044d4
bbiiss <bbiiss@mail.ru>
2022-01-29 20:39:23 +0300

    commit 7

--- src/test/java/BaseTests/BaseTests.java
+++ src/test/java/BaseTests/BaseTests.java
@@ -1,47 +1,19 @@
 package BaseTests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import com.google.common.io.Files;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 
-import java.io.File;
-import java.io.IOException;
-
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
-import static common.Values.BASE_URL;
 
 public class BaseTests {
 
-//    @BeforeAll
-/*    static void setupAllureReports() {
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        // либо для тонкой настройки:
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true)
-        );
-    }*/
-
-/*    public static void setupBrowser() {
-
-        System.setProperty("selenide.browser", "firefox"); // "chrome", "firefox", "ie", "htmlunit", "phantomjs", "opera"
-    }*/
-
-    //    @Step("Создаем подразделение {word}")
+    @Step
     public void createDepartment(String number) {
 //        Selenide.zoom(100);
         $(By.xpath("//*[@data-id=" + number + "]/div/button")).click();
         $(By.linkText("элемент структуры")).click();
     }
+
     public void createDepartmentLevels_1_5() {
         createDepartment("0");
         createDepartment("1");
