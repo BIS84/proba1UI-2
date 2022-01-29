ca965ede1ebc19f520fdb22a7cfb3cf3c89cd41d
bbiiss <bbiiss@mail.ru>
2022-01-29 21:45:37 +0300

    commit 10

--- src/test/java/createDepartmentsTest/Tests.java
+++ src/test/java/createDepartmentsTest/Tests.java
@@ -2,6 +2,7 @@ package createDepartmentsTest;
 
 import BaseTests.BaseTests;
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -10,6 +11,51 @@ import static com.codeborne.selenide.Selenide.*;
 import static common.Values.BASE_URL;
 
 public class Tests extends BaseTests {
+
+    @Step("Открыть ЛК")
+    public void openPage() {
+        open(BASE_URL);
+    }
+
+    //    @Step("Создать департамент")
+    public void createDepartment(String number) {
+//        Selenide.zoom(100);
+        $(By.xpath("//*[@data-id=" + number + "]/div/button")).click();
+        $(By.linkText("элемент структуры")).click();
+    }
+
+    @Step("Создать департаменты уровней 1 - 5")
+    public void createDepartmentLevels_1_5() {
+        createDepartment("0");
+        createDepartment("1");
+        createDepartment("2");
+        createDepartment("3");
+        createDepartment("4");
+    }
+
+    @Step("Создать департаменты уровня N")
+    public void createDepartmentLevel_N(String level) {
+        createDepartment(level);
+        createDepartment(level);
+        createDepartment(level);
+        createDepartment(level);
+        createDepartment(level);
+    }
+
+    @Step("Подождать 5 сек")
+    public void wait_5_sec() {
+        sleep(5000);
+    }
+
+    @Step("Проверить значение поля 'input'")
+    public void shouldInput() {
+        $(By.xpath("//*[@data-id=" + 3 + "]/div/div/div/input")).should(Condition.value("Управление 1"));
+    }
+
+    @Step("Проверить неверное значение поля 'input'")
+    public void shouldInputFailed() {
+        $(By.xpath("//*[@data-id=" + 3 + "]/div/div/div/input")).should(Condition.value("Управление 2"));
+    }
 
 
 
