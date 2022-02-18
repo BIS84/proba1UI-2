49995220f30dbea6d937e0f926e68050224f4a4f
bbiiss <bbiiss@mail.ru>
2022-02-18 03:10:16 +0300

    commit 2

---
+++ src/test/java/cloveri/Tests.java
@@ -0,0 +1,47 @@
+package cloveri;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+public class Tests {
+    static Properties properties;
+    static String host;
+    static String elements;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        properties = new Properties();
+        properties.load(new FileInputStream("src/test/resources/application.properties"));
+        host = properties.getProperty("host");
+        elements = properties.getProperty("elements");
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void getPositiveTest() {
+
+        given()
+//                .header("Autorization", "demo@cloveri.com", "jAamqBf2uPoS")
+                .log()
+                .ifValidationFails()
+                .filter(new AllureRestAssured())
+                .when()
+                .get(elements)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+
+}
